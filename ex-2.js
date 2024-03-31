f6cab6e9a892e9c5cd833f92341629bb1529132a
Teaboyd <ph.patiphan@kkumail.com>
2024-04-01 01:42:38 +0700

    Complete JavaScript Conditional Logic Assignment and debug

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,13 @@
 //Exercise 2
-let lightBulbStatus = "On";
+let lightBulbStatus = "Broken";
 
 // Start coding here.
+if (lightBulbStatus === "On"){
+    console.log("Light bulb is On.");
+}else if(lightBulbStatus === "Off"){
+    console.log("Light bulb is off.");
+}else if(lightBulbStatus === "Broken"){
+    console.log("Light bulb is Broken.");
+}else{
+    console.log("Plase choose the correct input (On/Off/Broken)");
+}
